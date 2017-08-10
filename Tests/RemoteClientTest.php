03d87e7eda04809b05c0d66b342af572b285d9b0
Samusevich Alexander <archer.developer@gmail.com>
2017-08-10 22:00:23 +0300

    RemoteClient test added

---
+++ Tests/RemoteClientTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: archer
+ * Date: 10.08.2017
+ * Time: 21:53
+ */
+
+namespace Garant\FilePreviewGeneratorBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class RemoteClientTest extends KernelTestCase
+{
+    public function indexTest()
+    {
+        dump('OK!');
+    }
+}
