00af0d34676db848aa21893fbef8988badd23308
Samusevich Alexander <archer.developer@gmail.com>
2017-08-16 20:11:34 +0300

    PowerPoint generator implemented

--- Tests/WordTest.php
+++ Tests/WordTest.php
@@ -11,28 +11,8 @@ namespace Garant\FilePreviewGeneratorBundle\Tests;
 
 use Garant\FilePreviewGeneratorBundle\Generator\AbstractGenerator;
 
-class OfficeTest extends AbstractGeneratorTest
+class Word extends AbstractGeneratorTest
 {
-    public function testXLSXToTXT()
-    {
-        $this->generate('test.xlsx', AbstractGenerator::PREVIEW_FORMAT_TEXT);
-    }
-
-    public function testXLSToJPEG()
-    {
-        $this->generate('test.xls', AbstractGenerator::PREVIEW_FORMAT_JPEG);
-    }
-
-    public function testXLSXToJPEG()
-    {
-        $this->generate('test.xlsx', AbstractGenerator::PREVIEW_FORMAT_JPEG);
-    }
-
-    public function testXLSXToHTML()
-    {
-        $this->generate('test.xlsx', AbstractGenerator::PREVIEW_FORMAT_HTML);
-    }
-
     public function testPlainToJPEG()
     {
         $this->generate('test.txt', AbstractGenerator::PREVIEW_FORMAT_JPEG);
